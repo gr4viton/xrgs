e803e503a08748c19d6c8362dd7a1d9b93eb23e5
刘鑫阳 <22374074@buaa.edu.cn>
2025-05-25 14:35:45 +0800

    clear submodule

--- 3rdparty/OpenXR.Loader.1.0.34/api_layers/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/aaaPROJECT/Chapter3/openxr/src/api_layers
-# Build directory: E:/aaaPROJECT/Chapter3/build2/_deps/openxr-build/src/api_layers
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
